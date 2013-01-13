7bc05b888133988632349006ea03f0485ef92661
Michael Charland <mike_ch_1@hotmail.com>
2013-01-13 10:50:01 -0500

    Now using roboguice

--- TenIdeas/gen/ca/charland/tenideas/TenIdeas/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ca.charland.tenideas.TenIdeas;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- TenIdeas/src/main/java/ca/charland/tenideas/TenIdeas/HelloAndroidActivity.java
+++ TenIdeas/src/main/java/ca/charland/tenideas/TenIdeas/HelloAndroidActivity.java
@@ -1,25 +1,39 @@
 package ca.charland.tenideas.TenIdeas;
 
+import com.google.inject.Inject;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectResource;
+import roboguice.inject.InjectView;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class HelloAndroidActivity extends Activity {
+public class HelloAndroidActivity extends RoboActivity {
 
-    private static String TAG = "TenIdeas";
+	@InjectView(R.id.name)
+	TextView name;
+	
+	@InjectView(R.id.thumbnail)
+	ImageView thumbnail;
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-		Log.i(TAG, "onCreate");
-        setContentView(R.layout.main);
-    }
+	@InjectResource(R.drawable.icon)
+	Drawable icon;
+
+	@InjectResource(R.string.app_name)
+	String myName;
+	
+	@Inject	
+	LocationManager loc;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		name.setText("Hello, " + myName);
+	}
 
 }
-
