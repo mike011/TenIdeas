1a5ccfb026b0f0df9bb9fd41a7bac36ad7f96a95
Michael Charland <mike_ch_1@hotmail.com>
2013-02-10 21:28:52 -0500

    still lots wrong

--- TenIdeas/src/main/java/ca/charland/tenideas/manage/DateListOfPreviousEntriesActivity.java
+++ TenIdeas/src/main/java/ca/charland/tenideas/manage/DateListOfPreviousEntriesActivity.java
@@ -1,13 +1,15 @@
 package ca.charland.tenideas.manage;
 
 import ca.charland.activity.manage.BaseDateListOfPreviousEntriesActivity;
+import ca.charland.db.BasicPersonDataTable;
+import ca.charland.db.DatabaseConnection;
 import ca.charland.db.DateListDataSource;
 
 public class DateListOfPreviousEntriesActivity extends BaseDateListOfPreviousEntriesActivity {
 
 	@Override
 	protected DateListDataSource getDateListDataSource() {
-		return new DateListDataSource(null);
+		return new DateListDataSource(new DatabaseConnection(this, DatabaseConnection.DATABASE_NAME, BasicPersonDataTable.TABLE_NAME, BasicPersonDataTable.CREATE_PEOPLE_TABLE));
 	}
 
 	@Override
