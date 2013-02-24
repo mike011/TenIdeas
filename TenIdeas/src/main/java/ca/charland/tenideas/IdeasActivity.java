a04c3da5bf1c4be6981e6c67d47d0b9343b9abe4
Michael Charland <mike_ch_1@hotmail.com>
2013-02-24 09:46:15 -0500

    basic ui setup

--- TenIdeas/src/main/java/ca/charland/tenideas/IdeasActivity.java
+++ TenIdeas/src/main/java/ca/charland/tenideas/IdeasActivity.java
@@ -1,7 +1,13 @@
 package ca.charland.tenideas;
 
+import android.os.Bundle;
 import roboguice.activity.RoboActivity;
 
 public class IdeasActivity extends RoboActivity {
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ideas);
+	}
 }
